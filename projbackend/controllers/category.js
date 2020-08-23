179bbf9c0f78e7cbea5508f4015cfc49a7c441f0
selvabharathi <selvabharathi327@gmail.com>
2020-08-23 12:41:08 +0530

    update and delete category writter

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -38,3 +38,30 @@ exports.getAllCategories = (req,res) => {
         res.json(categories)
     })
 }
+
+exports.updateCategory = (req,res) => {
+    const category = req.category
+    category.name = req.body.name
+    category.save((err,updatedCategory)=>{
+        if(err){
+            return res.status(400).json({
+                error: "unable to update category"
+            })
+        }
+        res.json(updatedCategory)
+    })
+}
+
+exports.removeCategory = (req,res) => {
+    const category = req.category
+    category.remove((err,category)=>{
+        if(err){
+            return res.status(400).json({
+                error: "unable to update category"
+            })
+        }
+        res.json({
+            message : `removed category - ${category.name} successfully`
+        })
+    })
+}
--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var router = express.Router()
 
-const {getCategoryById,createCategory,getCategory,getAllCategories} = require("../controllers/category")
+const {getCategoryById,createCategory,getCategory,getAllCategories,updateCategory,removeCategory} = require("../controllers/category")
 const {isSignedIn,isAuthenticated,isAdmin} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
 
@@ -10,8 +10,14 @@ router.param("userId",getUserById)
 router.param("categoryId",getCategoryById)
 
 //actual routes goes here
+//create
 router.post("/category/create/:userId",isSignedIn,isAuthenticated,isAdmin,createCategory)
+//read
 router.get("/category/:categoryId",getCategory)
 router.get("/categories",getAllCategories)
+//update
+router.put("/category/:categoryId/:userId",isSignedIn,isAuthenticated,isAdmin,updateCategory)
+//delete
+router.delete("/category/:categoryId/:userId",isSignedIn,isAuthenticated,isAdmin,removeCategory)
 
 module.exports = router

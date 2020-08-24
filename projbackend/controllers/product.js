e02f2c277dbd46527e0892c46227075823223771
selvabharathi <selvabharathi327@gmail.com>
2020-08-24 21:05:17 +0530

    getProduct(optimisation for retriving photo), deleteProduct, updateProduct, getAllProduct were writtern

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -58,3 +58,89 @@ exports.createProduct = (req,res) => {
         })
     })
 }
+
+exports.getProduct = (req,res) => {
+    req.product.photo = undefined
+    return res.json(req.product)
+}
+
+//middleware-done for performance optimization in fetching photo's
+exports.photo = (req,res,next) => {
+    if(req.product.photo.data){
+        res.set("Content-Type",req.product.photo.contentType)
+        return res.send(req.product.photo.data) 
+    }
+    next()
+}
+
+
+exports.deleteProduct = (req,res) => {
+    let product = req.product
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            return res.status(400).json({
+                error: "Failed to delete the product"
+            })
+        }
+        res.json({
+            message: "Deletion was a success",deletedProduct
+        })
+    })
+}
+
+exports.updateProduct = (req,res) => {
+    let form = formidable.IncomingForm()
+    form.keepExtensions = true
+
+    form.parse(req,(err,fields,file)=>{
+        if(err){
+            return res.status(400).json({
+                error: "problem with image"
+            })
+        }
+        
+        //updating product
+        let product = req.product
+        product = _.extend(product,fields)
+
+        //handle file here
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error: "File size too big"
+                })
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type
+        }
+        
+        // save to the db
+        product.save((err,product)=>{
+            if(err){
+                res.status(400).json({
+                    error: "updating tshirt in db failed"
+                })
+            }
+            res.json(product)
+        })
+    })
+}
+
+//product listing 
+exports.getAllProduct = (req,res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 8
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id"
+    Product.find()
+    .select("-photo")
+    .populate("category")
+    .sort([[sortBy, "asc"]])
+    .limit(limit)
+    .exec((err,products)=>{
+        if(err){
+            return res.status(400).json({
+                error: "no product found"
+            })
+        }
+        res.json(products)
+    })
+}
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-const {getProductById,createProduct} = require("../controllers/product")
+const {getProductById,createProduct,getProduct,photo,deleteProduct,updateProduct,getAllProduct} = require("../controllers/product")
 const {isAdmin,isAuthenticated,isSignedIn} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
 
@@ -9,6 +9,16 @@ const {getUserById} = require("../controllers/user")
 router.param("userId",getUserById)
 router.param("productId",getProductById)
 //actual routes
+//create route
 router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct)
+//read route
+router.get("/product/:productId",getProduct)
+router.get("/product/photo/:productId",photo)
+//delete route
+router.delete("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,deleteProduct)
+//update route
+router.put("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,updateProduct)
+//listing route
+router.get("/products",getAllProduct)
 
 module.exports = router

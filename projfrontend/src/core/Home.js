81fee5f1e47f41f3eb689e30141d732bf6a09561
selvabharathi <selvabharathi327@gmail.com>
2020-09-01 17:33:16 +0530

    adding styles css to project

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,9 @@
 import React from "react"
-
+import "../styles.css"
 export default function Home() {
     return (
         <div>
-            <h1>Hello front end</h1>
+            <h1 className="text-white">Hello front end</h1>
         </div>
     )
 }

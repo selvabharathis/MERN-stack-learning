deb09a72ede055d73259722fe72639f9a63c6c5a
selvabharathi <selvabharathi327@gmail.com>
2020-09-02 15:06:34 +0530

    creating base component for font end

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,11 +1,23 @@
 import React from "react"
 import "../styles.css"
 import {API} from "../backend"
+import Base from "./Base"
+
 export default function Home() {
     console.log("API IS",API)
     return (
-        <div>
-            <h1 className="text-white">Hello front end</h1>
-        </div>
+        <Base title="Home page">
+            <div className="row">
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+                <div className="col-4">
+                    <button className="btn btn-success">TEST</button>
+                </div>
+            </div>
+        </Base>
     )
 }

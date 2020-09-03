5a909eedde4f05925a280aab3c4549b5067998c9
selvabharathi <selvabharathi327@gmail.com>
2020-09-03 15:44:04 +0530

    added description

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -6,7 +6,7 @@ import Base from "./Base"
 export default function Home() {
     console.log("API IS",API)
     return (
-        <Base title="Home page">
+        <Base title="Home page" description="Welcome to t-shirt store">
             <div className="row">
                 <div className="col-4">
                     <button className="btn btn-success">TEST</button>

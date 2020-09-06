e25621773a3470f41eca8b02f0417618646a65c1
selvabharathi <selvabharathi327@gmail.com>
2020-09-06 15:24:37 +0530

    creating call from front end to backend for category and product, also CRUD operation for product from front end request has been added

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -1,7 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import Base from "../core/Base";
+import { isAuthenticated } from "../auth/helper";
+import { Link } from "react-router-dom";
+import { createCategory } from "./helper/adminapicall";
 
 const AddCategory = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const { user, token } = isAuthenticated();
+
+  const goBack = () => (
+    <div className="mt-5">
+      <Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">
+        Admin Home
+      </Link>
+    </div>
+  );
+
+  const handleChange = (event) => {
+    setError("");
+    setName(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setError("");
+    setSuccess(false);
+    //backend req fired
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(true);
+      } else {
+        setError("");
+        setSuccess(true);
+        setName("");
+      }
+    });
+  };
+
+  const successMessage = () => {
+    if (success) {
+      return <h4 className="text-success">Category created successfully</h4>;
+    }
+  };
+
+  const warningMessage = () => {
+    if (error) {
+      return <h4 className="text-success">Failed to create category</h4>;
+    }
+  };
+
+  const myCategoryForm = () => (
+    <form action="">
+      <div className="form-group">
+        <p className="lead">Enter the category</p>
+        <input
+          type="text"
+          className="form-control my-3"
+          onChange={handleChange}
+          value={name}
+          autoFocus
+          required
+          placeholder="For Ex. Summer"
+        />
+        <button onClick={onSubmit} className="btn btn-outline-info">
+          Create Category
+        </button>
+      </div>
+    </form>
+  );
+
   return (
     <Base
       title="Create a category here"
@@ -10,7 +80,7 @@ const AddCategory = () => {
     >
       <div className="row bg-white rounded">
         <div className="col-md-8 offset-md-2">
-          <h1>hello</h1>
+          {successMessage()} {warningMessage()} {myCategoryForm()} {goBack()}
         </div>
       </div>
     </Base>
---
+++ projfrontend/src/admin/helper/adminapicall.js
@@ -0,0 +1,99 @@
+import { API } from "../../backend";
+
+// category calls
+export const createCategory = (userId, token, category) => {
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//get all categories
+export const getAllCategories = () => {
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//products calls
+// create a product
+export const createProduct = (userId, token, product) => {
+  return fetch(`${API}/product/create/${userId}`, {
+    method: "POST",
+    header: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+// get all products
+export const getAllProducts = () => {
+  return fetch(`${API}/products`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//delete a product
+export const deleteProduct = (productId, userId, token) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "DELETE",
+    header: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//get a product
+export const getProduct = (productId) => {
+  return fetch(`${API}/product/${productId}`, {
+    method: "GET",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
+
+//update a product
+export const updateProduct = (productId, userId, token, product) => {
+  return fetch(`${API}/product/${productId}/${userId}`, {
+    method: "PUT",
+    header: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: product,
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
